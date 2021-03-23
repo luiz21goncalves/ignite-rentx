6e009b9f0b780cc65cb12ae099ff805173c3d9b0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-03-23 20:58:07 -0300

    refactor: specification entity

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,10 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuid } from "uuid";
 
+@Entity("specifications")
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
+
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
