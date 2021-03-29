76cc9f6a0462991e45e744d991e50d4d374976f9
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-03-29 20:09:24 -0300

    feat: add user repository im memory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryImMemory.ts
@@ -0,0 +1,47 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  private users: User[] = [];
+
+  async create({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      password,
+      driver_license,
+    });
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async save(user: User): Promise<User> {
+    const userIndex = this.users.findIndex(
+      (findUser) => findUser.id === user.id
+    );
+
+    this.users[userIndex] = user;
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
