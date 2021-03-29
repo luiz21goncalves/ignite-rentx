28d4c79b2399f82f89c4cdf243004454c8b0dea1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-03-29 19:05:07 -0300

    feat: add category repository im memory

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name);
+  }
+}
+
+export { CategoriesRepositoryInMemory };
