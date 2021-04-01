9bcadf27e908922f04f1f8632822d64b80d725b2
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-04-01 18:59:11 -0300

    feat: add seed create user admin

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,24 @@
+import { hash } from "bcrypt";
+import { v4 as uuid } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuid();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO users(id, name, email, password, driver_license, "isAdmin", created_at, updated_at)
+    VALUES('${id}', 'Admin', 'admin@rentx.com.br', '${password}', '000123', true, 'now()', 'now()')
+    `
+  );
+
+  await connection.close();
+}
+
+create()
+  .then(() => console.log("User admin created!"))
+  .catch((err) => console.error(err))
+  .finally(() => console.log("Query finalizada!"));
