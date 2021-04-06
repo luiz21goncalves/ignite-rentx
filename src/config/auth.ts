93d4e4fd003e6fc78c0062d90b5b2dc5cd684f7a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-04-06 20:57:44 -0300

    feat: add auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  secret_token: "84a7bfa74967c74819208acb9934e458",
+  expires_in_token: "15m",
+  secret_refresh_token: "e47fb962b69a5b557327f3d7e2f780f6",
+  expires_in_refresh_token: "2d",
+  expires_refresh_token_days: 2,
+};
+
+export { authConfig };
