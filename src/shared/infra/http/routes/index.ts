6be2139ff0d2351cc613a5a18bc30a1348938972
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-04-18 13:09:38 -0300

    fix: add authenticate routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { passwordRoutes } from "./password.routes";
@@ -15,5 +16,6 @@ routes.use("/users", usersRoutes);
 routes.use("/cars", carsRoutes);
 routes.use("/rentals", rentalsRoutes);
 routes.use("/password", passwordRoutes);
+routes.use(authenticateRoutes);
 
 export { routes };
